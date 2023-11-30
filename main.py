28c7f0711c9a89cb9be6b9cf0165476294660bde
JesseIguess <blueberry21007@gmail.com>
2023-11-30 23:19:57 +0000

    Chanhged extractign text to functionf orm

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
-import urllib.request  
+import urllib.request
+import re
 from bs4 import BeautifulSoup 
 
 # url = "https://www.foxnews.com/world/hamas-releases-more-israeli-hostages-6th-day-cease-fire"
 # url = "https://www.newsmax.com/us/joe-biden-impeachment-house/2023/11/29/id/1144091/"
-url = "https://www.cnn.com/2023/11/29/politics/vivek-ramaswamy-aide-trump-campaign/index.html"
-omitted_paragraph_keywords = ["all rights reserved", "subscribe", "newsletter", "@"]
+# url = "https://www.cnn.com/2023/11/29/politics/vivek-ramaswamy-aide-trump-campaign/index.html"
+omitted_paragraph_keywords = ["all rights reserved", "subscribe", "newsletter", "@", "©", "(c)"]
+seperating_punctuation = ".?<>()[];:\"!"
 
-html = urllib.request.urlopen(url) 
-htmlParse = BeautifulSoup(html, "html.parser") 
-
-for para in htmlParse.find_all("p"): 
-    text = para.get_text()
-    text_lower = text.lower()
+def extractText(url, keep_newlines = true):
+    html = urllib.request.urlopen(url) 
+    html_parse = BeautifulSoup(html, "html.parser")
     
-    omit = False
-    for word in omitted_paragraph_keywords:
-        if word in text_lower:
-            omit = True
-            break
+    text = ""
+    for para in html_parse.find_all("p"): 
+        para_text = para.get_text()
+        para_text_lower = para_text.lower()
+        
+        # Omit paragraphs with certain keywords
+        omit = False
+        for word in omitted_paragraph_keywords:
+            if word in para_text_lower:
+                omit = True
+                break
+        
+        if omit: continue
+        text += para_text + "[NEWLINEHEREEEEE]"
     
-    if omit: continue
+    if keep_newlines:
+        # Remove excess newlines
+        text = text.replace("\n", "").replace("[NEWLINEHEREEEEE]", "\n")
+    else:
+        text = text.replace("\n", "").replace("[NEWLINEHEREEEEE]", "")
 
-    print("    " + text)
+    return text
+
+def corroborate(url1, url2):
+    article1 = extractText(url1)
+    article2 = extractText(url2)
+
+    # impleeemtnt laterrrrrr
+
+print(extractText("https://www.cnn.com/2023/11/29/politics/vivek-ramaswamy-aide-trump-campaign/index.html"))
