9e8d8e674627040c38f2d6c324e78bc6f8b744bf
JesseIguess <blueberry21007@gmail.com>
2023-12-04 23:09:34 +0000

    i like oily black men

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import urllib.request
 import re
+import random
+import spacy
 from bs4 import BeautifulSoup 
 
 # url = "https://www.foxnews.com/world/hamas-releases-more-israeli-hostages-6th-day-cease-fire"
 # url = "https://www.newsmax.com/us/joe-biden-impeachment-house/2023/11/29/id/1144091/"
 # url = "https://www.cnn.com/2023/11/29/politics/vivek-ramaswamy-aide-trump-campaign/index.html"
-omitted_paragraph_keywords = ["all rights reserved", "subscribe", "newsletter", "@", "©", "(c)"]
-seperating_punctuation = ".?<>()[];:\"!"
 
-def extractText(url, keep_newlines = true):
+nlp = spacy.load("en_core_web_sm")
+
+omitted_paragraph_keywords = ["all rights reserved", "subscribe", "newsletter", "@", "©", "(c)"]
+
+margin = 2
+
+def extractText(url):
     html = urllib.request.urlopen(url) 
     html_parse = BeautifulSoup(html, "html.parser")
     
@@ -25,20 +31,68 @@ def extractText(url, keep_newlines = true):
                 break
         
         if omit: continue
-        text += para_text + "[NEWLINEHEREEEEE]"
+        text += para_text + "[NEWPARA]"
     
-    if keep_newlines:
-        # Remove excess newlines
-        text = text.replace("\n", "").replace("[NEWLINEHEREEEEE]", "\n")
-    else:
-        text = text.replace("\n", "").replace("[NEWLINEHEREEEEE]", "")
+    # Remove excess newlines
+    text = text.replace("\n", "")
 
     return text
 
-def corroborate(url1, url2):
-    article1 = extractText(url1)
-    article2 = extractText(url2)
+# ==========================================================================
 
+def processData(text):
+    paragraphs = text.split("[NEWPARA]")
+    paragraph_data = []
+
+
+    paragraph_index = 0
+    for paragraph in paragraphs:
+        document = nlp(paragraph)
+        paragraph_data.append({
+            "subjects": [],
+            "subject_vectors": spacy.vectors.Vectors(shape=(10000, 300))
+        })
+
+        for token in document:
+            if token.is_space: continue
+            if token.dep_ == "nsubj": # if subject
+                paragraph_data[paragraph_index]["subjects"].append(token)
+                paragraph_data[paragraph_index]["subject_vectors"].add(token.text)
+        paragraph_index += 1
+
+    # document = nlp(text)
+    # vectors = spacy.vectors.Vectors(shape=(10000, 300))
+
+    # for token in document:
+    #     vectors.add(token.text)
+        # print(token.text)
+    # for token in document:
+    #     if token.is_space: continue
+    #     print(f"{token}: {token.dep_}", end="")
+    #     if token.is_sent_end:
+    #         print(" [yes end sentence wopooo]")
+    #     else:
+    #         print("")
+
+    return data
+
+# ==========================================================================
+
+
+
+def identifyPhrases(text):
+    phrases = []
+
+def plugAndPlay(text):
+    pass
+
+def corroborate(text1, text2):
+    return generateSequence("the", 10, text1 + text2, "balls")
     # impleeemtnt laterrrrrr
 
-print(extractText("https://www.cnn.com/2023/11/29/politics/vivek-ramaswamy-aide-trump-campaign/index.html"))
+# print(corroborate(
+#     extractText("https://www.cnn.com/2023/11/29/politics/vivek-ramaswamy-aide-trump-campaign/index.html"),
+#     extractText("https://www.newsmax.com/us/joe-biden-impeachment-house/2023/11/29/id/1144091/")
+# ))
+
+processData("I love oily black men. I like hurling bricks at children, which is an illegal crime. Please kill yourself now. Etc. is shorthand for etcetera!")
