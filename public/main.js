8714ea05987f65ac5b837c66950f5fcff81c854c
JesseIguess <blueberry21007@gmail.com>
2023-12-16 00:25:35 +0000

    Added notice and article layour

--- public/main.js
+++ public/main.js
@@ -1,11 +1,18 @@
+function closeWindow() {
+    document.getElementsByClassName('notice-bg')[0].classList.toggle('hidden');
+}
+
 function article(_article) {
     return `
         <div class="row article-thumbnail lighter">
             <div class="col-3" style="background-color: var(--background-3); background-image: url(${_article.urlToImage});"></div>
             <div class="col" style="padding-left: 50px;">
                 <h3>${_article.title}</h3>
-                <button class="accent">View</button>&nbsp;&nbsp;&nbsp;
-                <a href=${_article.url}>View original</a><br/><br/>
+                <a href="/corroborate/${btoa(_article.url)}">
+                    <button class="accent">View</button>
+                </a>
+                &nbsp;&nbsp;&nbsp;
+                <a href="${_article.url}">View original</a><br/><br/>
                 <p>Source: ${_article.source.name}<br/>By ${_article.author}</p>
             </div>
         </div>
