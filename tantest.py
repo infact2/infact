9d08ba191ea0c713e87fb08a7a90ad75a1743ce9
Tanush Aggarwal <epictanush@gmail.com>
2024-02-14 13:28:53 -0800

    google custom search engine api call added

--- tantest.py
+++ tantest.py
@@ -5,13 +5,13 @@ def getDaLinks(prompt):
        
        
        prompt += "&"
-       apiKey = AIzaSyDqabrP5CbkUciLP9nr4o_9XKDtNiwp5hs 
-       searchEngineID = d6c2539d1fec44866 #cx
+       apiKey = 'AIzaSyDqabrP5CbkUciLP9nr4o_9XKDtNiwp5hs'
+       searchEngineID = "d6c2539d1fec44866" #cx
        page = 1 # using the first page
        start = (page - 1) * 10 + 1 # calculating start, (page=2) => (start=11), (page=3) => (start=21)
 
 
-       url = 'https://www.googleapis.com/customsearch/v1?key={apiKey}&cx={searchEngineID}&q={prompt}&start={start}'
+       url = 'https://www.googleapis.com/customsearch/v1?key=' + apiKey + '&cx=' + searchEngineID +'&q=' +prompt + '&start=' + str(start)
 
        parameters = {
               'cx': "d6c2539d1fec44866",
@@ -24,31 +24,34 @@ def getDaLinks(prompt):
        }
 
        response = requests.get(url).json()
-
+       #print(response)
        # get the result items
-       search_items = data.get("items")
+       search_items = response.get("items")
        # iterate over 10 results found
-       for i, search_item in enumerate(search_items, start=1):
-              try:
-                     long_description = search_item["pagemap"]["metatags"][0]["og:description"]
-              except KeyError:
-                     long_description = "N/A"
-              # get the page title
-              title = search_item.get("title")
-              # page snippet
-              snippet = search_item.get("snippet")
-              # alternatively, you can get the HTML snippet (bolded keywords)
-              html_snippet = search_item.get("htmlSnippet")
-              # extract the page url
-              link = search_item.get("link")
-              # print the results
-              print("="*10, f"Result #{i+start-1}", "="*10)
-              print("Title:", title)
-              print("Description:", snippet)
-              print("Long description:", long_description)
-              print("URL:", link, "\n")
+       if search_items is not None:
+              for i, search_item in enumerate(search_items, start=1):
+                     try:
+                            long_description = search_item["pagemap"]["metatags"][0]["og:description"]
+                     except KeyError:
+                            long_description = "N/A"
+                     # get the page title
+                     title = search_item.get("title")
+                     # page snippet
+                     snippet = search_item.get("snippet")
+                     # alternatively, you can get the HTML snippet (bolded keywords)
+                     html_snippet = search_item.get("htmlSnippet")
+                     # extract the page url
+                     link = search_item.get("link")
+                     # print the results
+                     print("="*10, f"Result #{i+start-1}", "="*10)
+                     print("Title:", title)
+                     print("Description:", snippet)
+                     print("Long description:", long_description)
+                     print("URL:", link, "\n")
+       else:  
+              print("provide a different prompt, this had no results")
 
-getDaLinks("poopoo")
+getDaLinks("trump")
 
 #getLinks(["San", "Diego", "homeless"])
 #test push 3
