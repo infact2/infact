01378f9da44d1a08a03961ae362a9ffcbb30b865
Tanush Aggarwal <epictanush@gmail.com>
2024-02-14 14:27:33 -0800

    changing search engine parameters

--- googlesearchengineapi.py
+++ googlesearchengineapi.py
@@ -3,18 +3,19 @@ import requests
 def getDaLinks(prompt):
        print("Given Prompt: " + prompt + "\n")
        
-       
-       prompt += "&"
+       #parameters
        apiKey = 'AIzaSyDqabrP5CbkUciLP9nr4o_9XKDtNiwp5hs'
        searchEngineID = "d6c2539d1fec44866" #cx
        page = 1 # using the first page
        start = (page - 1) * 10 + 1 # calculating start, (page=2) => (start=11), (page=3) => (start=21)
 
-
        url = f'https://www.googleapis.com/customsearch/v1?key={apiKey}&cx={searchEngineID}&q={prompt}&start={str(start)}&tbm=news'
+       
        print(url)
+       
        response = requests.get(url).json()
        #print(response)
+       
        # get the result items
        search_items = response.get("items")
        # iterate over 10 results found
@@ -27,16 +28,16 @@ def getDaLinks(prompt):
                      # get the page title
                      title = search_item.get("title")
                      # page snippet
-                     snippet = search_item.get("snippet")
+                     #snippet = search_item.get("snippet")
                      # alternatively, you can get the HTML snippet (bolded keywords)
-                     html_snippet = search_item.get("htmlSnippet")
+                     #html_snippet = search_item.get("htmlSnippet")
                      # extract the page url
                      link = search_item.get("link")
                      # print the results
                      print("="*10, f"Result #{i+start-1}", "="*10)
                      print("Title:", title)
-                     print("Description:", snippet)
-                     print("Long description:", long_description)
+                     #print("Description:", snippet)
+                     #print("Long description:", long_description)
                      print("URL:", link, "\n")
        else:  
               print("provide a different prompt, this had no results")
