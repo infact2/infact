c66911e8f8c0da5285f8b7f0f89e5354ad08808b
JesseIguess <blueberry21007@gmail.com>
2024-07-09 07:01:52 +0000

    added quota to headlines

--- headlines.py
+++ headlines.py
@@ -20,30 +20,43 @@ _newsapi = NewsApiClient(api_key=os.getenv("NEWS_API_KEY"))
 
 class Headlines:
     cache = []
+    quota = 8
 
     def getTopHeadlines(self):
         return self.cache
 
     def setTopHeadlines(self):
         print("Resetting headlines...")
-        category = "WORLD"
 
-        top_headlines = _newsapi.get_top_headlines()
         new_cache = []
+        unskipped_articles = 0
+        page = 1
+        while unskipped_articles < self.quota:
+            try:
+                top_headlines = _newsapi.get_top_headlines(page=page)
+            except:
+                print("results limited")
+                break
 
-        # check if the request was successful
-        if top_headlines["status"] == "ok":
-            articles = top_headlines["articles"]
-            # Print the titles and URLs of the top headlines
-            cur = 0
-            for article in articles:
-                if isPolitical(article["title"]):
-                    new_cache.append(article)
-                    print(f"LOADED: {cur}/{len(articles)} articles")
-                else: print(f"LOADED: {cur}/{len(articles)} articles SKIPPED")
-                cur += 1
-        else:
-            print(f"Error fetching top headlines: {top_headlines['message']}")
+            # check if the request was successful
+            if top_headlines["status"] == "ok":
+                articles = top_headlines["articles"]
+                # Print the titles and URLs of the top headlines
+                cur = 0
+                for article in articles:
+                    if unskipped_articles >= self.quota:
+                        print("quota met (f)")
+                        break # if article quota met, break.
+                    if isPolitical(article["title"]):
+                        new_cache.append(article)
+                        unskipped_articles += 1
+                        print(f"LOADED: {cur}/{len(articles)} articles (t: {unskipped_articles})")
+                    else: print(f"LOADED: {cur}/{len(articles)} articles (t: {unskipped_articles}) [SKIPPED]")
+                    cur += 1
+            else:
+                print(f"Error fetching top headlines: {top_headlines['message']}")
+                break # stop searching if error
+            page += 1
         self.cache = new_cache
 
     async def interval(self):
